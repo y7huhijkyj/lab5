1f429e3e70c05edef63da00fc7fa172183632c12
Mark H <mmhwang@ucsd.edu>
2016-02-04 21:59:32 +0000

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -4,4 +4,13 @@ exports.addFriend = function(req, res) { 
 	// Your code goes here
 	console.log("sdlkfjdslfksjf");
 	res.render('add');
+
+	var myObject = new Object();
+
+	myObject.name = req.query.name;
+	myObject.description = req.query.description;
+	myObject.imageURL = "http://lorempixel.com/500/500/people";
+	console.log(JSON.stringify(myObject));
+
+	data["friends"].push(myObject);
  }
